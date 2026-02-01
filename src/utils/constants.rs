b34199c7aaeebbc31058e0aff9dd35661280f842
Emii-lia <fiaromiangaly@gmail.com>
2026-02-01 15:06:53 +0300

    refactor(utils): moving constants to utils modules

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -3,7 +3,7 @@ const REPO_NAME: &str = "yewi-kit";
 const REPO_BRANCH: &str = "master";
 const RAW_GITHUB_URL: &str = "https://raw.githubusercontent.com";
 
-pub(crate) fn get_repo_config() -> (String, String, String, String) {
+pub fn get_repo_config() -> (String, String, String, String) {
     (
         REPO_OWNER.to_string(),
         REPO_NAME.to_string(),
