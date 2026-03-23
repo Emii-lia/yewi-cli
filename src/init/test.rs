0904112020df33acbd724acdc9d6592ded82e173
Emii-lia <fiaromiangaly@gmail.com>
2026-03-23 16:39:14 +0300

    test(theme): implementing test for creating project with custom hex theme

--- src/init/test.rs
+++ src/init/test.rs
@@ -3,6 +3,7 @@ use std::fs;
 use std::fs::remove_dir_all;
 use std::path::PathBuf;
 use crate::init::create;
+use crate::utils::shade::{shades_of, ShadeKey};
 
 fn temp_project() -> (PathBuf, PathBuf) {
   let dir = temp_dir();
@@ -18,7 +19,7 @@ fn create_fails_if_dir_already_exists() {
   let result = create(
     project.to_str().unwrap(),
     Some("slate".into()),
-    Some(false)
+    Some(true)
   );
   assert!(result.is_err());
   assert!(result.unwrap_err().to_string().contains("already exists"));
@@ -30,11 +31,11 @@ fn create_rejects_invalid_theme() {
   let result = create(
     project.to_str().unwrap(),
     Some("invalid-color".into()),
-    Some(false)
+    Some(true)
   );
 
   assert!(result.is_err());
-  assert!(result.unwrap_err().to_string().contains("Invalid theme color"));
+  assert!(result.unwrap_err().to_string().contains("Invalid hex color"));
   assert!(!project.exists());
 }
 
@@ -45,7 +46,7 @@ fn produces_expected_project_structure() {
   create(
     project.to_str().unwrap(),
     Some("sky".into()),
-    Some(false)
+    Some(true)
   ).unwrap();
 
   assert!(project.join("src").exists());
@@ -97,5 +98,33 @@ fn create_with_i18n_produces_expected_project_structure() {
   let cargo_toml = fs::read_to_string(project.join("Cargo.toml")).unwrap();
   assert!(cargo_toml.contains("i18nrs"));
 
+  assert!(remove_dir_all(&project).is_ok());
+}
+
+#[test]
+fn create_with_custom_hex_theme() {
+  let project = temp_dir().join("yew-test-custom-theme");
+  let hex = "#eb9868";
+  create(
+    project.to_str().unwrap(),
+    Some(hex.into()),
+    Some(true)
+  ).unwrap();
+
+  let shades = shades_of(hex).unwrap();
+
+  assert!(project.join("src").join("styles").join("main.scss").exists());
+  let style = fs::read_to_string(project.join("src").join("styles").join("main.scss")).unwrap();
+
+  for (shade, value) in shades {
+    match shade {
+      ShadeKey::U(key) => {
+        assert!(style.contains(&format!("--primary-{}: {}", key, value)));
+      }
+      ShadeKey::Default => {
+        assert!(style.contains(&format!("--primary: {}", value)));
+      }
+    }
+  }
   assert!(remove_dir_all(&project).is_ok());
 }
